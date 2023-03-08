22ea9af35e0d13c3b78834b69b07b2ce837a00c5
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-03-08 17:53:10 +0300

    Time: 64 ms (34.72%), Space: 18.4 MB (36.59%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -11,13 +11,13 @@ class Solution:
         
         def traverse(root):
             nonlocal max_
+            
             if not root:
                 return
 
             traverse(root.left)
             dict[root.val] = 1 + dict.get(root.val, 0)
-            if max_ < dict[root.val]:
-                max_ = max(max_, dict[root.val])
+            max_ = max(max_, dict[root.val])
             
             traverse(root.right)
             
