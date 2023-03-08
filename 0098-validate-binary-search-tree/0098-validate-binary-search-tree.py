5c1ec303d6aa10edef7031b2c2ecc657650f7a49
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-03-08 14:17:43 +0300

    Time: 50 ms (43.97%), Space: 17.6 MB (11.85%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -7,33 +7,34 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        def valid(root, left, right):
+        # def valid(root, left, right):
+        #     if not root:
+        #         return True
+        #     if not (root.val > left and root.val < right):
+        #         return False
+        #     return valid(root.left, left, root.val) and valid(root.right, root.val, right)
+        # return valid(root, float(-inf), float(inf))
+
+        
+        lst = []
+        
+        def traverse(root):
             if not root:
-                return True
-            if not (root.val > left and root.val < right):
-                return False
-            return valid(root.left, left, root.val) and valid(root.right, root.val, right)
-        return valid(root, float(-inf), float(inf))
+                return
 
-        
-#         lst = []
-        
-#         def traverse(root):
-#             if not root:
-#                 return
-
-#             traverse(root.left)
-#             lst.append(root.val)
-#             traverse(root.right)
+            traverse(root.left)
+            lst.append(root.val)
+            traverse(root.right)
             
-#         traverse(root) 
+        traverse(root) 
         
-#         lst2 = sorted(lst)
+        lst2 = sorted(lst)
+        mySet = set(lst2)
 
-#         if lst == lst2:
-#             return True
-#         else:
-#             return False
+        if lst == lst2 and len(mySet) == len(lst2):
+            return True
+        else:
+            return False
         
             
                 
