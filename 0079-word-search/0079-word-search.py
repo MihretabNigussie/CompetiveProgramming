e54b351cbe50405fb1d0d9393fb7c090e32d9eb6
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-03-10 16:04:58 +0300

    Time: 7996 ms (44.67%), Space: 14 MB (45.20%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,30 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        rows, cols = len(board) , len(board[0])
+        path = set()
+        
+        def helper(row, col, i):
+            if i == len(word):
+                return True
+            if ((row < 0) or (col < 0) or 
+            (row >= rows) or (col >= cols)
+            or (word[i]  != board[row][col]) or
+            ((row, col) in path)):
+                return False
+            
+            path.add((row, col))
+            
+            res = (helper(row + 1, col, i+1) or
+            helper(row-1, col, i+1) or
+            helper(row, col + 1, i + 1) or
+            helper(row, col - 1, i + 1) )
+            
+            path.remove((row, col))
+            return res
+        for row in range(rows):
+            for col in range(cols):
+                if helper(row, col, 0):
+                    return True
+        return False
+                
