745aeb6ae3cb01402db0ec4f8c64f54136dbaf36
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-04-12 14:59:57 +0300

    Time: 156 ms (29.53%), Space: 17.8 MB (16.36%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -15,9 +15,9 @@ class Solution:
             return 1 +  dfs(i, j + 1) + dfs(i -1, j) + dfs(i + 1, j) + dfs(i, j - 1)
 
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] :
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] and grid[i][j] not in visited: 
                     area = max(area ,dfs(i,j))
         return area
         
