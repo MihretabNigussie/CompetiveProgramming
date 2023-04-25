9a48fde7cda9655ba2c687c03cd0723034c45d7b
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-04-25 17:27:08 +0300

    Time: 72 ms (88.47%), Space: 14.2 MB (10.19%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -9,18 +9,19 @@ class Solution:
             
             if (i,j) in visited or i >= len(image) or j >= len(image[0]) or i <0 or j < 0:
                 return
+            
             visited.add((i,j))
+            
             if image[i][j] == temp:
+                
                 image[i][j] = color
+                
                 dfs(i, j + 1)
                 dfs(i -1, j)
                 dfs(i + 1, j)
                 dfs(i, j - 1)
+                
                 return 
-            
-            
-            return image
-        
         
         dfs(sr, sc)
         image[sr][sc] = color
