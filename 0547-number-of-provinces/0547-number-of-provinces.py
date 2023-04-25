8e6d7d76d2a01d9c0a61c13dbb81d545d5ab05eb
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-04-25 19:23:47 +0300

    Time: 184 ms (78.44%), Space: 14.5 MB (37.94%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -3,28 +3,27 @@ class Solution:
         
         graph = defaultdict(list)
         count = 0
+        visited = set()
         
         for i in range(len(isConnected)):
             for j in range(len(isConnected[0])):
                 if isConnected[i][j]:
                     graph[i].append(j)
-    
-        
-        def dfs_recursive_disconnected(graph):
-            nonlocal count
-            visited = set()
-            for start in graph:
-                if start not in visited:
-                    count += 1
-                    dfs_recursive_helper(graph, start, visited)
- 
-        def dfs_recursive_helper(graph, start, visited):
+
+        def dfs_recursive(graph, start):
+            
             visited.add(start)
+
             for neighbor in graph[start]:
                 if neighbor not in visited:
-                    dfs_recursive_helper(graph, neighbor, visited)
+                    dfs_recursive(graph, neighbor)
+            return visited
 
-        
-        dfs_recursive_disconnected(graph)
+        for node in graph:
+            if node not in visited:
+                count += 1
+                dfs_recursive(graph, node)
         return count
+
+                    
         
