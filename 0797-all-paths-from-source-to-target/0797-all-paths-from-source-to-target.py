846b69c628086c57a26fd14000699705f9f912e5
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-05-22 11:52:01 +0300

    Time: 116 ms (15.95%), Space: 18.3 MB (26.62%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,16 +1,9 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         
-        graph_ = defaultdict(list)
-        lst =  []
         visited = set()
         max_  = len(graph) - 1
-        
-        for i in range(len(graph)):
-            for j in range(len(graph[i])):
-                if graph[i]:
-                    graph_[i].append(graph[i][j])
-        
+       
         def all_paths(graph, start, end, path=[]):
             path = path + [start]
             
@@ -29,7 +22,7 @@ class Solution:
             return paths
 
         
-        return all_paths(graph_, 0, max_)
+        return all_paths(graph, 0, max_)
         
        
             
