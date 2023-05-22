1ebaaf3c6aeac37cec2b8d385099965fe11e8423
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-05-22 10:52:19 +0300

    Time: 89 ms (25.10%), Space: 16.5 MB (13.85%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,27 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
-        visited = set()
+        
         temp = image[sr][sc]
         
         def dfs(i , j):
             nonlocal temp
             
-            if (i,j) in visited or i >= len(image) or j >= len(image[0]) or i <0 or j < 0:
+            if i >= len(image) or j >= len(image[0]) or i <0 or j < 0 or image[i][j] != temp or image[i][j] == color:
                 return
             
-            visited.add((i,j))
-            
-            if image[i][j] == temp:
                 
-                image[i][j] = color
-                
-                dfs(i, j + 1)
-                dfs(i -1, j)
-                dfs(i + 1, j)
-                dfs(i, j - 1)
-                
-                return 
+            image[i][j] = color
+
+            dfs(i, j + 1)
+            dfs(i -1, j)
+            dfs(i + 1, j)
+            dfs(i, j - 1)
+
+            return 
         
         dfs(sr, sc)
         image[sr][sc] = color
