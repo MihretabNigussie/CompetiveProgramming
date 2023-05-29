91f692dac159ce90046533094cc464419f017c47
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-05-30 00:00:51 +0300

    Time: 36 ms (60.42%), Space: 16.4 MB (9.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,10 +3,10 @@ class Solution:
         num1, num2 = 1,1
         
         for i in range(n-1):
+           
+            num1 , num2 = num2, num1
+            num1  = num1 + num2
             
-            temp = num1
-            num1 = num1 + num2
-            num2 = temp
         return num1
             
         
