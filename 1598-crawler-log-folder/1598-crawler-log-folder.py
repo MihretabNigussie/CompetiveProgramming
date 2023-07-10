64b3e0698176111eadc67cc504d0a489dd90ea69
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-07-10 16:03:36 +0300

    Time: 62 ms (66.42%), Space: 16.6 MB (10.15%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        
+        stack = []
+        
+        for log in logs:
+            
+            if log == '../':
+                if stack:
+                    stack.pop()
+            elif log == './':
+                continue
+            else:
+                stack.append('c')
+            
+        return len(stack)
+        
+        
