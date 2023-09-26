364912ec748f190fcd066bc528589c75f622f12d
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-09-26 12:01:20 +0300

    Time: 43 ms (48.38%), Space: 16.4 MB (23.75%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,9 +1,66 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        res = ""
-        for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
-                    return res
-            res += strs[0][i]
-        return res
+        
+        trie = Trie()
+        
+        shortestStr = min(strs, key = len)
+        trie.insert(shortestStr)
+        
+        ans = len(shortestStr)
+        
+        
+        for string in strs:
+            
+            ans = min(ans, trie.search(string))
+        
+        return shortestStr[:ans]
+            
+            
+
+        
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        
+        current = self.root
+        for char in word:
+            
+            idx = ord(char) - ord('a')
+            
+            if not current.children[idx]:
+                
+                current.children[idx] = TrieNode()
+            current = current.children[idx]
+        current.is_end = True
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        for index , char in enumerate(word):
+            idx = ord(char) - ord('a')
+            
+            if not current.children[idx]:
+
+                return index
+            current = current.children[idx]
+        
+        return len(word)
+                
+        
+
+    
+        
+        
+class TrieNode:
+    def __init__(self):
+        self.is_end = False
+        self.children = [None for _ in range(26)]
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
