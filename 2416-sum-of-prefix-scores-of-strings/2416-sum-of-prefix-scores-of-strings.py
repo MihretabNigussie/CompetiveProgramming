108797c13b7938bd9e16849f6760636ce70d2fa0
Mihretab Nigussie <110567717+MihretabNigussie@users.noreply.github.com>
2023-10-04 01:17:14 -0700

    Time: 6961 ms (5.01%), Space: 494.6 MB (31.77%) - LeetHub

---
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -0,0 +1,53 @@
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        output = []
+        for word in words:
+                output.append(trie.search(word))
+           
+        return output
+    
+class TrieNode:
+    def __init__(self):
+        self.children = [None for _ in range(26)]
+        self.is_end = False
+        self.count = 0
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+        
+
+    def insert(self, word: str) -> None:
+        
+        curr =  self.root
+
+        for char in word:
+            idx = ord(char) - ord('a')
+            if curr.children[idx]:
+                curr.children[idx].count += 1
+            else:
+                newNode = TrieNode()
+                curr.children[idx] = newNode
+                newNode.count += 1
+            curr = curr.children[idx]
+        curr.is_end =  True
+        
+        
+
+    def search(self, word: str) -> bool:
+        self.curr = self.root
+        temp = 0
+        for char in word:
+            idx = ord(char) - ord('a')
+            if self.curr.children[idx] != None:
+                temp += self.curr.count
+                self.curr =  self.curr.children[idx]
+            else:
+                return 0
+        temp += self.curr.count
+        return temp
